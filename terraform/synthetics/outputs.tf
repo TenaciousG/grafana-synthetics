0a18a9b3f1791b9c0329e695d5169b7c6704881a
Glen <glen.olsen@gmail.com>
2026-04-21 14:43:43 +0200

    dummy2

--- terraform/synthetics/outputs.tf
+++ terraform/synthetics/outputs.tf
@@ -3,6 +3,7 @@ output "synthetic_browser_monitoring_check_id" {
   value       = grafana_synthetic_monitoring_check.Synthetics_BrowserCheck_login
 }
 
+
 output "synthetic_http_monitoring_check_id" {
   description = "The ID of the created synthetic monitoring check."
   value       = grafana_synthetic_monitoring_check.Synthetics_HttpCheck
